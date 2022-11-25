8afa2c531c7a2802f07d8aa840247e6e68df9f0c
YanmingFu <2106869299@qq.com>
2022-11-25 10:32:21 +0800

    第二次test测试

--- gitTest/src/main/java/com/fym/ABC.java
+++ gitTest/src/main/java/com/fym/ABC.java
@@ -4,5 +4,9 @@ public class ABC {
     public static void main(String[] args) {
         System.out.println("The second git test");
         System.out.println("i put this gitTest into my first repository");
+        System.out.println("i put this gitTest into my first repository");
+        System.out.println("i put this gitTest into my first repository");
+
+
     }
 }
